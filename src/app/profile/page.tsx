e24cdda36ddca98080dd9e57a260f242ce79edc2
bradleygichuru <bradleygichuru@gmail.com>
2024-05-08 22:27:08 +0300

    feat:adds loading skeletons

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Stat, StatLabel, StatNumber, Wrap, WrapItem } from "@chakra-ui/react";
+import { SkeletonCircle, SkeletonText, Stat, StatLabel, StatNumber, Wrap, WrapItem } from "@chakra-ui/react";
 import { signIn, useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -15,12 +15,14 @@ import {
 } from "utils/used-types";
 export default function ProfilePage() {
   const callBackUrl = usePathname();
+  const [loading, setLoading] = useState(false);
   const { data: session, status } = useSession();
   if (status === "unauthenticated") {
     void signIn(undefined, { callbackUrl: callBackUrl });
   }
   const [userData, setUserData] = useState<UserDataAl>();
   useEffect(() => {
+    setLoading(true);
     fetch("/api/get-user-data", {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email: session?.user?.email }),
@@ -30,8 +32,12 @@ export default function ProfilePage() {
       .then((data: { status: string; data: UserData }) => {
         console.log(data);
         setUserData(data.data);
+        setLoading(false);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
   }, [session]);
   return (
     <div>
@@ -42,11 +48,12 @@ export default function ProfilePage() {
           </div>
         </div>
         <div className="m-2 flex flex-col">
-          <h1>profile</h1>
+          <SkeletonText isLoaded={!loading}>
+            <h1>profile</h1>
 
-          <span className="text-3xl">{userData?.userrole == "pharmacy" || userData?.userrole == "clinic" ? userData?.estname : `${userData?.firstname} ${userData?.lastname}`}</span>
+            <span className="text-3xl">{userData?.userrole == "pharmacy" || userData?.userrole == "clinic" ? userData?.estname : `${userData?.firstname} ${userData?.lastname}`}</span></SkeletonText>
 
-          <span className="mt-2">{userData?.userrole}</span>
+          <SkeletonText isLoaded={!loading}> <span className="mt-2">{userData?.userrole}</span></SkeletonText>
         </div>
       </div>
       <div>
@@ -54,54 +61,77 @@ export default function ProfilePage() {
           {userData?.userrole == "pharmacy" || userData?.userrole == "clinic" ? (
 
             <WrapItem>
-              <Stat className="m-3 rounded-md border-2 p-2">
-                <StatLabel>Establishment Name</StatLabel>
-                <StatNumber>{userData?.estname}</StatNumber>
-              </Stat>
+              <SkeletonText isLoaded={!loading}>
+                <Stat className="m-3 rounded-md border-2 p-2">
+                  <StatLabel>Establishment Name</StatLabel>
+                  <StatNumber>{userData?.estname}</StatNumber>
+                </Stat>
+              </SkeletonText>
             </WrapItem>
 
           ) : (<>
             <WrapItem>
+              <SkeletonText isLoaded={!loading}>
 
-              <Stat className="m-3 rounded-md border-2 p-2">
-                <StatLabel>First Name</StatLabel>
-                <StatNumber>{userData?.firstname}</StatNumber>
-              </Stat>
+                <Stat className="m-3 rounded-md border-2 p-2">
+                  <StatLabel>First Name</StatLabel>
+                  <StatNumber>{userData?.firstname}</StatNumber>
+                </Stat>
+              </SkeletonText>
             </WrapItem>
             <WrapItem>
-              <Stat className="m-3 rounded-md border-2 p-2">
-                <StatLabel>Last Name</StatLabel>
-                <StatNumber>{userData?.lastname}</StatNumber>
-              </Stat>
+              <SkeletonText isLoaded={!loading}>
+
+                <Stat className="m-3 rounded-md border-2 p-2">
+                  <StatLabel>Last Name</StatLabel>
+                  <StatNumber>{userData?.lastname}</StatNumber>
+                </Stat>
+              </SkeletonText>
             </WrapItem></>
           )}
         </Wrap>
-        <Stat className="m-3 rounded-md border-2 p-2">
-          <StatLabel>Phone Number</StatLabel>
-          <StatNumber>{userData?.phonenumber}</StatNumber>
-        </Stat>
-        {userData?.userrole == "doctor" || userData?.userrole == 'clinician' ? (<Stat className="m-3 rounded-md border-2 p-2">
-          <StatLabel>Primary Area of Speciality</StatLabel>
-          <StatNumber>{userData?.primaryareaofspeciality}</StatNumber>
-        </Stat>
+        <SkeletonText isLoaded={!loading}>
+
+          <Stat className="m-3 rounded-md border-2 p-2">
+            <StatLabel>Phone Number</StatLabel>
+            <StatNumber>{userData?.phonenumber}</StatNumber>
+          </Stat>
+        </SkeletonText>
+        {userData?.userrole == "doctor" || userData?.userrole == 'clinician' ? (
+          <SkeletonText isLoaded={!loading}>
+
+            <Stat className="m-3 rounded-md border-2 p-2">
+              <StatLabel>Primary Area of Speciality</StatLabel>
+              <StatNumber>{userData?.primaryareaofspeciality}</StatNumber>
+            </Stat>
+          </SkeletonText>
         ) : ('')}
 
         {userData?.userrole == "pharmacy" || userData?.userrole == "clinic" ? (
-          <Stat className="m-3 rounded-md border-2 p-2">
-            <StatLabel>Location</StatLabel>
-            <StatNumber>{userData?.location}</StatNumber>
-          </Stat>
+          <SkeletonText isLoaded={!loading}>
+
+            <Stat className="m-3 rounded-md border-2 p-2">
+              <StatLabel>Location</StatLabel>
+              <StatNumber>{userData?.location}</StatNumber>
+            </Stat>
+          </SkeletonText>
         ) : (
-          <Stat className="m-3 rounded-md border-2 p-2">
-            <StatLabel>County of Practice</StatLabel>
-            <StatNumber>{userData?.countyofpractice}</StatNumber>
-          </Stat>
+          <SkeletonText isLoaded={!loading}>
+
+            <Stat className="m-3 rounded-md border-2 p-2">
+              <StatLabel>County of Practice</StatLabel>
+              <StatNumber>{userData?.countyofpractice}</StatNumber>
+            </Stat>
+          </SkeletonText>
         )}
         {userData?.userrole == "pharmacy" || userData?.userrole == "clinic" ? (
-          <Stat className="m-3 rounded-md border-2 p-2">
-            <StatLabel>Name</StatLabel>
-            <StatNumber>{userData?.estname}</StatNumber>
-          </Stat>
+          <SkeletonText isLoaded={!loading}>
+
+            <Stat className="m-3 rounded-md border-2 p-2">
+              <StatLabel>Name</StatLabel>
+              <StatNumber>{userData?.estname}</StatNumber>
+            </Stat>
+          </SkeletonText>
         ) : (''
         )}
       </div>
