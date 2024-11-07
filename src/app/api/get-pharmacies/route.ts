af6c9376b090d4b2715ead34a0675e964d0e19ba
bradleygichuru <bradleygichuru@gmail.com>
2024-11-07 16:04:18 +0300

    feat:only show verified pharmacies

--- src/app/api/get-pharmacies/route.ts
+++ src/app/api/get-pharmacies/route.ts
@@ -1,10 +1,13 @@
-
+import { eq } from "drizzle-orm";
 import { pharmacy } from "drizzle/schema";
 import { db, pool } from "utils/db-pool";
 
 export async function GET(request: Request) {
   try {
-    const getPharmacies = await db.select().from(pharmacy);
+    const getPharmacies = await db
+      .select()
+      .from(pharmacy)
+      .where(eq(pharmacy.verified, true));
 
     if (getPharmacies && getPharmacies.length >= 0) {
       console.log(getPharmacies);
